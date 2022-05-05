fdb1ad18e9a7d778cbe0ebe48ad59a92db4294c0
Prashant Kumar <kp84582@gmail.com>
2022-05-05 13:27:32 +0530

    space removed

--- src/components/Homepage/Testimonial.js
+++ src/components/Homepage/Testimonial.js
@@ -22,79 +22,82 @@ const Testimonial = () => {
             </h2>
           </div>
           <Swiper
-              slidesPerView={4}
-              spaceBetween={30}
-              slidesPerGroup={1}
-              loop={true}
-              // autoplay={{
-              //     delay: 2500,
-              //     disableOnInteraction: false,
-              //   }}
-              loopFillGroupWithBlank={true}
-              pagination={{
-                clickable: true,
-              }}
-              breakpoints={{
-                340: {
-                  slidesPerView: 1,
-                },
-                768: {
-                  slidesPerView: 2,
-                },
-                1024: {
-                  slidesPerView: 3,
-                },
-              }}
-             // navigation={true}
-             // modules={[Pagination, Navigation]}
-              //className="mySwiper"
-            >
-          {testimonialData.map((curElem) => {
-            const {icon, title, auth_img, description, name, designation} =
-              curElem;
-            return (
-              <>
-              <SwiperSlide>
-                <div
-                  className="testimonial-active-02"
-                  data-aos="fade-up"
-                  data-aos-duration="1000"
-                >
-                  <div className="swiper">
-                    <div className="swiper-wrapper card">
-                      <div className="swiper-slide card-body">
-                        <div className="testimonial-item bg-white">
-                          <div className="testimonial-quote-icon"><img src={icon} alt='icon' /></div>
-                          <div className="testimonial-main-content">
-                            <div className="testimonial-caption">
-                              <h3 className="testimonial-caption__title">
-                                {title}
-                              </h3>
-                              <p>{description}</p>
-                            </div>
-                            <div className="testimonial-info">
-                              <div className="testimonial-info__image">
-                                <img src={auth_img} alt="Avatar" />
+            slidesPerView={4}
+            spaceBetween={30}
+            slidesPerGroup={1}
+            loop={true}
+            // autoplay={{
+            //     delay: 2500,
+            //     disableOnInteraction: false,
+            //   }}
+            loopFillGroupWithBlank={true}
+            pagination={{
+              clickable: true,
+            }}
+            breakpoints={{
+              340: {
+                slidesPerView: 1,
+              },
+              768: {
+                slidesPerView: 2,
+              },
+              1024: {
+                slidesPerView: 3,
+              },
+            }}
+            // navigation={true}
+            // modules={[Pagination, Navigation]}
+            //className="mySwiper"
+          >
+            {testimonialData.map((curElem) => {
+              const { icon, title, auth_img, description, name, designation } =
+                curElem;
+              return (
+                <>
+                  <SwiperSlide>
+                    <div
+                      className="testimonial-active-02"
+                      data-aos="fade-up"
+                      data-aos-duration="1000"
+                    >
+                      <div className="swiper">
+                        <div className="swiper-wrapper card">
+                          <div className="swiper-slide card-body">
+                            <div className="testimonial-item bg-white">
+                              <div className="testimonial-quote-icon">
+                                <img src={icon} alt="icon" />
                               </div>
-                              <div className="testimonial-info__caption">
-                                <h5 className="testimonial-info__name">{name}</h5>
-                                <p className="testimonial-info__designation">
-                                  {designation}
-                                </p>
+                              <div className="testimonial-main-content">
+                                <div className="testimonial-caption">
+                                  <h3 className="testimonial-caption__title">
+                                    {title}
+                                  </h3>
+                                  <p>{description}</p>
+                                </div>
+                                <div className="testimonial-info">
+                                  <div className="testimonial-info__image">
+                                    <img src={auth_img} alt="Avatar" />
+                                  </div>
+                                  <div className="testimonial-info__caption">
+                                    <h5 className="testimonial-info__name">
+                                      {name}
+                                    </h5>
+                                    <p className="testimonial-info__designation">
+                                      {designation}
+                                    </p>
+                                  </div>
+                                </div>
                               </div>
                             </div>
                           </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                </div>
-                </SwiperSlide>
-              </>
-             
-            );
-          })}
-           </Swiper>
+                  </SwiperSlide>
+                </>
+              );
+            })}
+          </Swiper>
         </div>
 
         <div className="testimonial-section__shape-01" data-depth="-0.5"></div>
